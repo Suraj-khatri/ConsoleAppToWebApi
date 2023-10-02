57347466617110e58b121582b355b2a318cd7de5
Suraj <surajpoudelkhatri@gmail.com>
2023-10-02 11:17:22 +0545

    2nd commit

---
+++ ConsoleToWebApi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleToWebApi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
